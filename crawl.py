17d2bf02b011818a020377faf24379f3e84286b6
null_lambda <jeoffrey0522@naver.com>
2020-01-10 21:05:37 +0900

    more info

--- crawl.py
+++ crawl.py
@@ -7,7 +7,7 @@ import re
 import os
 
 # to extract:
-#   posts = {idx: url, title}
+#   posts = {idx: url, title, reg_date, content, view, voteup, votedown}
 #   comments = {idx: list of (reg_date, name, ip or user_id, content)}
 
 def check_robots(url):
@@ -21,45 +21,57 @@ gall_id = 'dbd' # gallery id
 n_page = 1 # number of board pages to scrap
 posts = {}
 
+# get post url and info from board
 t_start = datetime.datetime.now()
 for page in range(1, n_page + 1):
     url_board = f"https://gall.dcinside.com/mgallery/board/lists/?id={gall_id}&page={page}"
-    if page > 1: 
-        print('\r', end='')
+    #if page > 1: 
+    #    print('\r', end='')
     print(f'fetching posts - page {page}/{n_page}, {url_board}', end='')
 
     url_board = f"https://gall.dcinside.com/mgallery/board/lists/?id={gall_id}&page={page}"
     if not check_robots(url_board):
         print(f'request blocked by robots.txt: {url_board}')
         continue
-
     headers = {
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
     }
     try:
         req = requests.get(url_board, headers=headers)
-        html = req.text
-
-        soup = BeautifulSoup(html, 'lxml')
-        data = soup.find('table','gall_list').find_all('tr', {'class':'ub-content us-post'})
-
-        for post in data:
-            if(post['data-type']=='icon_notice'):
+        soup = BeautifulSoup(req.text, 'lxml')
+        data_list = soup.find('table','gall_list').find_all('tr', {'class': 'ub-content us-post'})
+        for data in data_list:
+            if(data['data-type']=='icon_notice'):
                 continue
-            idx = post['data-no']
-            a = post.find('td', {'class':'gall_tit ub-word'}).a
-            post_url = 'https://gall.dcinside.com/' + a['href']
-            post_title = a.get_text()
-            post_reg_date = post.find('td', {'class':'gall_date'})['title']
-            posts[idx] = {'url': post_url, 'title': post_title, 'reg_date': post_reg_date}
-            #print(posts[idx])
+            idx = data['data-no']
+            a = data.find('td', {'class':'gall_tit ub-word'}).a
+            post = {}
+            post['url'] = 'https://gall.dcinside.com/' + a['href']
+            post['title'] = a.get_text()
+            post['reg_date'] = data.find('td', {'class': 'gall_date'})['title']
+            post['view'] = int(data.find('td', {'class': 'gall_count'}).get_text())
+            post['voteup'] = int(data.find('td', {'class': 'gall_recommend'}).get_text())
+    
+            # open post url and get info 
+            try:
+                req = requests.get(post['url'], headers=headers)
+                soup = BeautifulSoup(req.text, 'lxml')
+                post['content'] = str(soup.find('div', {'class': 'writing_view_box'}).find('div', {'style':'overflow:hidden;'}))
+                post['votedown'] = int(soup.find('p', {'class': 'down_num'}).get_text())
+                posts[idx] = post
+            except:
+                continue
     except:
         continue
+
+
+
 t_end = datetime.datetime.now()
 print(f'\rfetching posts - Done. {t_end - t_start} elapsed.'.ljust(100))
 
 
+# group posts into chunks  
 t_start = datetime.datetime.now()
 post_chunks = {}
 for idx in posts:
@@ -68,6 +80,8 @@ for idx in posts:
         post_chunks[idx_hash] = {}
     post_chunks[idx_hash][idx] = posts[idx]
 
+
+# get comments and save each chunk(posts, comments) to file
 i = 1
 for idx_hash, post_chunk in sorted(post_chunks.items())[::-1]:
     comments = {}
@@ -76,7 +90,7 @@ for idx_hash, post_chunk in sorted(post_chunks.items())[::-1]:
             print('\r', end='')
         print(f'fetching comments - post {i}/{len(posts)}, {post["url"]}, {post["title"]}', end='')
         i += 1
-        
+
         comments[idx] = []
         comment_url = 'https://gall.dcinside.com/board/comment/'
         comment_page = 1
@@ -92,8 +106,7 @@ for idx_hash, post_chunk in sorted(post_chunks.items())[::-1]:
             }
             try:
                 req = requests.post(comment_url, data = data, headers=headers)
-                json_text = req.text
-                data = json.loads(json_text)
+                data = json.loads(req.text)
 
                 if comment_page == 1:
                     re_match = re.compile(r'viewComments\((\d),').findall(data['pagination'])
@@ -115,7 +128,7 @@ for idx_hash, post_chunk in sorted(post_chunks.items())[::-1]:
                 pass
             #print(comments[idx])
             comment_page += 1
-    # save post chunk (with comments) to file
+    # save chunk to file
     filename =  f'data/{gall_id}_{idx_hash}xxx.json'
     if not os.path.exists(os.path.dirname(filename)):
         os.makedirs(os.path.dirname(filename))
